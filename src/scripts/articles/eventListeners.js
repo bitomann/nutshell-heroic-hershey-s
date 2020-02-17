fb12cf884095d36945eeb6fdd6793f26472cdd23
Kathryn Wohl <ktwohl@gmail.com>
2020-02-17 13:39:50 -0600

    added current user id placeholder for eventual auth functionality

--- src/scripts/articles/eventListeners.js
+++ src/scripts/articles/eventListeners.js
@@ -7,13 +7,14 @@ const newsTitleInput = document.querySelector("#articleTitle")
 const newsUrlInput = document.querySelector("#articleUrl")
 const newsSynopsisInput = document.querySelector("#articleSynopsis")
 const newsOutputSection = document.querySelector("#newsOutputContainer")
+const currentUserId = parseInt(sessionStorage.getItem("activeUser"))
 
 const newsEvents = {
     addSaveEventListener() {
         saveButton.addEventListener("click", () => {
             newsOutputSection.innerHTML = ""
             const newArticle = {
-                "userId": Math.floor((Math.random() * 10) + 1),
+                "userId": currentUserId,
                 "url": newsUrlInput.value,
                 "title": newsTitleInput.value,
                 "synopsis": newsSynopsisInput.value,
