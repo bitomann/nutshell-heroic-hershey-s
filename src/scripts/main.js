876ef2020c692c5a7b2bb1374c64ee197a5cb73d
Cooper Nichols <coopnich92@gmail.com>
2020-02-17 15:55:28 -0600

    save button for tasks is functional

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ import apiActions from "./events/eventsDataHandler.js";
 import domOperations from "./events/eventsDomHandler.js";
 import newEventButton from "./events/eventFormListener.js";
 import saveEventButton from "./events/saveEventListener.js"
-
+import taskEvents from "./tasks/tasksEventListeners.js"
 /*
     Import all the tools into main.js that are needed to display
     the initial UI to the user. Either the login form should appear
@@ -27,5 +27,5 @@ newEventButton.eventButtonFunction()
 saveEventButton.saveEventFunction()
 
 //tasks section tools - Cooper Nichols
-import taskEvents from "./tasks/tasksEventListeners.js"
 taskEvents.renderAllTasks()
+taskEvents.createTask()
