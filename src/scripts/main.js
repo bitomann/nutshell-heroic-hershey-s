e729d1513f43fea0b1cad31b97a1f6304ee4ff28
Cooper Nichols <coopnich92@gmail.com>
2020-02-17 10:30:33 -0600

    added currentUserId variable to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@
     or the dashboard should be rendered.
 */
 
+//in your main.js (until you reach the log in ticket)
+sessionStorage.setItem("activeUser", 1)
+//wherever you need to use the id of the user that is logged in, you will reference the variable currentUserId
+const currentUserId = parseInt(sessionStorage.getItem("activeUser"))
+
 const message = "Time to build an application that gives you all the information you need in a Nutshell"
 
 document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
