b2be3e9c7fcdeaf15506226e862da02a8d3177cd
Kathryn Wohl <ktwohl@gmail.com>
2020-02-18 22:02:25 -0600

    added function to sort saved news articles by timestamp in descending order

--- src/scripts/articles/domManager.js
+++ src/scripts/articles/domManager.js
@@ -5,6 +5,9 @@ const newsContainer = document.querySelector("#newsOutputContainer")
 
 const renderNewsArticles = (articles) => {
     newsContainer.textContent = ""
+    articles.sort(function(a, b) {
+        return a.timestamp - b.timestamp
+    })
     articles.forEach(article => {
         const articleHtml = makeNewsComponent(article)
         newsContainer.innerHTML += articleHtml
