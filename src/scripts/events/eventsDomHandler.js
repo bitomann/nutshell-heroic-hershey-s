c85fcc3c7531ed870efeeb855dcc84353aab466c
Matt Reeds <mattlreeds@gmail.com>
2020-02-19 11:58:53 -0600

    working on parsing the date

--- src/scripts/events/eventsDomHandler.js
+++ src/scripts/events/eventsDomHandler.js
@@ -15,11 +15,8 @@ console.log(Date.parse(dateString))
 console.log(Date.parse("2020-1-20"))
 console.log(Date.parse("2020-3-20"))
 
-// get value of date input, parse date, sort entries, create new object to include date, conditional to check if this object equals the other, display only if 
-
-const comparingDates = {
-
-}
+/* get value of date input, parse date, sort entries, create new object to include date, 
+conditional to check if this object equals the other, display only if*/ 
 
 const domOperations = {
     eventEntryFactory: (eventName, date, location) => {
@@ -28,16 +25,25 @@ const domOperations = {
             date: date,
             location: location
         }
+        const userDate = Date.parse(date);
+        
+        
         return newEventObject;
     },
-
+    
     renderEventEntries: (entries) => {
         const newArray = entries.reverse();
         entryContainer.innerHTML = "";
         newArray.forEach(entry => {
             entryContainer.innerHTML +=
-                eventComponentObj.eventCardComponent(entry);
+            eventComponentObj.eventCardComponent(entry);
             // add a conditional statement that applies the required styling to upcoming event
+        })
+    },
+    comparingDates: (dates) => {
+        dates.forEach(date => {
+            Date.parse(date)
+            console.log(dates)
         })
     }
 }
