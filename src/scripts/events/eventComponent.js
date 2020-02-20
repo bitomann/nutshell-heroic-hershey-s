e81390dcb3d6840c7482a97b92854c5916a5a770
Matt Reeds <mattlreeds@gmail.com>
2020-02-20 10:06:47 -0600

    actually added styling this time

--- src/scripts/events/eventComponent.js
+++ src/scripts/events/eventComponent.js
@@ -3,12 +3,14 @@
 const eventComponentObj = {
     eventCardComponent: (eventEntry) => {
         return `
+        <div class="eventEntry">
         <section class="event-entry--${eventEntry.id}"></section>
             <section>${eventEntry.eventName}</section>
             <section class="dateOfEvent">${eventEntry.date}</section>
             <section>${eventEntry.location}</section>
-            <button id="editEvent--${eventEntry.id}">Edit Event</button>
+            <button class="editBtn" id="editEvent--${eventEntry.id}">Edit Event</button>
             <button id="deleteEvent--${eventEntry.id}">Delete Event</button>
+            </div>
         `
     }
 }
